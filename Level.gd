00fea26c8c9c106322cc111af00a44ccbc33eceb
noahstelmak <noah.bussmann@gmail.com>
2025-03-20 16:15:51 -0300

    fix gitignore

--- Level.gd
+++ Level.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 @export var level_name : String
 
-@export var suneiko : SuneikoTreisMill
+@export var suneiko : Snaker3000
 
 
 func _on_deathzone_player_is_ded() -> void:
--- deathzone.gd
+++ deathzone.gd
@@ -3,6 +3,6 @@ extends Area3D
 signal player_is_ded
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is SuneikoTreisMill:
+	if body is Snaker3000:
 		player_is_ded.emit()
 		body.got_dunked()
